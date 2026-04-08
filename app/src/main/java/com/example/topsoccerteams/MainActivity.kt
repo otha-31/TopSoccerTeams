0f505417b901dbc27654b8bf552d6a573eb8ea65
otha-31 <othavatela2@gmail.com>
2026-04-08 08:56:35 +0200

    added top 5 soccer teams

---
+++ app/src/main/java/com/example/topsoccerteams/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.topsoccerteams
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    // Teams from https://footballdatabase.com/ranking/south-africa/1
+    //retrieved on 21 May2022
+    val teams = arrayOf<String>("Mamelodi Sundowns FC",
+        "Bidvest Wits",
+        "Orlando Pirates",
+        "Kaizer Chiefs",
+        "Cape Town City FC")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
